6bd38a4f5197d789754e70b4c3a1e16c78d21351
kantoi <Kent@antikode.com>
2021-05-21 16:45:33 +0700

    Add HeadlessFirefox

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -8,40 +8,54 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-public class UITest 
-{
-    @Test(enabled=false)
-    public void startBrowser(){
-        System.setProperty("webdriver.gecko.driver","/Users/kentvanlim/NetBeansProjects/compass/driver/geckodriver");
+public class UITest {
+
+    @Test(enabled = false)
+    public void startBrowser() {
+        System.setProperty("webdriver.gecko.driver", "/Users/kentvanlim/NetBeansProjects/compass/driver/geckodriver");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setHeadless(true);
-        FirefoxDriver driver =new FirefoxDriver();
-        
-        driver.get("https://www.google.co.in");   
+        FirefoxDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.google.co.in");
     }
 	@Parameters("Browser")
-	@Test
-	public void startBrowser(String browserName)
-	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			 driver=new FirefoxDriver();
-		}
 
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-		driver.quit();
-	}
+    @Test(enabled = true)
+    public void startBrowser(String browserName) {
+        System.out.println("Parameter value is " + browserName);
+        WebDriver driver = null;
+
+        if (browserName.contains("Firefox")) {
+//            FirefoxBinary firefoxBinary = new FirefoxBinary();
+//            firefoxBinary.addCommandLineOptions("--headless");
+//            FirefoxOptions firefoxOptions = new FirefoxOptions();
+//            firefoxOptions.setBinary(firefoxBinary);
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setHeadless(true);
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver(firefoxOptions);
+        }
+
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+        driver.quit();
+    }
+
+   @Test(enabled=false)
+    public void startBrowser2(String browserName) {
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        FirefoxDriver driver = new FirefoxDriver();
+        new FirefoxDriver(firefoxOptions);
+
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
 }
-        
-	
-	
-
