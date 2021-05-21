eadc33b2e9f19ec02187c55cf9273a5b3f8519a7
kantoi <Kent@antikode.com>
2021-05-21 15:49:29 +0700

    Initiate

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -8,32 +8,39 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class UITest 
 {
-
-	@Parameters("Browser")
-	@Test
-	public void startBrowser(String browserName)
-	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			 driver=new ChromeDriver();
-		}
-		else if(browserName.contains("Edge"))
-		{
-			WebDriverManager.edgedriver().setup();
-			 driver=new EdgeDriver();
-		}
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-		driver.quit();
+    @Test
+    public void startBrowser(){
+        System.setProperty("webdriver.gecko.driver","/Users/kentvanlim/NetBeansProjects/compass/driver/geckodriver");
+        FirefoxDriver driver =new FirefoxDriver();
+        driver.get("https://www.google.co.in");   
+    }
+//	@Parameters("Browser")
+//	@Test
+//	public void startBrowser(String browserName)
+//	{
+//		System.out.println("Parameter value is "+browserName);
+//		WebDriver driver=null;
+//		
+//		if(browserName.contains("Chrome"))
+//		{
+//			WebDriverManager.chromedriver().setup();
+//			 driver=new ChromeDriver();
+//		}
+//		else if(browserName.contains("Edge"))
+//		{
+//			WebDriverManager.edgedriver().setup();
+//			 driver=new EdgeDriver();
+//		}
+//		driver.manage().window().maximize();
+//		driver.get("https://opensource-demo.orangehrmlive.com/");
+//		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+//		driver.quit();
 	}
+        
 	
 	
-}
+
