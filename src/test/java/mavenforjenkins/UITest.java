007c13f3813b66ee2864cefa58b0ec50641b0292
kantoi <Kent@antikode.com>
2021-05-21 16:25:59 +0700

    Modif Pom

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -9,37 +9,38 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class UITest 
 {
-    @Test
+    @Test(enabled=false)
     public void startBrowser(){
         System.setProperty("webdriver.gecko.driver","/Users/kentvanlim/NetBeansProjects/compass/driver/geckodriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setHeadless(true);
         FirefoxDriver driver =new FirefoxDriver();
+        
         driver.get("https://www.google.co.in");   
     }
-//	@Parameters("Browser")
-//	@Test
-//	public void startBrowser(String browserName)
-//	{
-//		System.out.println("Parameter value is "+browserName);
-//		WebDriver driver=null;
-//		
-//		if(browserName.contains("Chrome"))
-//		{
-//			WebDriverManager.chromedriver().setup();
-//			 driver=new ChromeDriver();
-//		}
-//		else if(browserName.contains("Edge"))
-//		{
-//			WebDriverManager.edgedriver().setup();
-//			 driver=new EdgeDriver();
-//		}
-//		driver.manage().window().maximize();
-//		driver.get("https://opensource-demo.orangehrmlive.com/");
-//		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-//		driver.quit();
+	@Parameters("Browser")
+	@Test
+	public void startBrowser(String browserName)
+	{
+		System.out.println("Parameter value is "+browserName);
+		WebDriver driver=null;
+		
+		if(browserName.contains("Firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			 driver=new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
 	}
+}
         
 	
 	
